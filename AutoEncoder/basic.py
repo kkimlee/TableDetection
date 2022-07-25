ab85db4e0ea602cd63e46a815b8dc987a579cbb0
kkimlee <40078267+kkimlee@users.noreply.github.com>
2022-07-25 13:05:25 +0900

    Create basic.py
    기본 autoencoder

---
+++ AutoEncoder/basic.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
+#%% 데이터셋 로드
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+print(x_train.shape)
+print(x_test.shape)
+
+#%% 모델 정의
+latent_dim = 64
+
+class Autoencoder(Model):
+    def __init__(self, encoding_dim):
+        super(Autoencoder, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = tf.keras.Sequential([
+            layers.Flatten(),
+            layers.Dense(latent_dim, activation='relu'),
+        ])
+        self. decoder = tf.keras.Sequential([
+            layers.Dense(784, activation='sigmoid'),
+            layers.Reshape((28, 28))
+        ])
+        
+    def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+autoencoder = Autoencoder(latent_dim)
+
+#%% 모델 초기화
+autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
+
+#%% 모델 학습
+autoencoder.fit(x_train, x_train,
+                epochs=10,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+#%% 테스트
+encoded_imgs = autoencoder.encoder(x_test).numpy()
+decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+
+#%% 테스트 결과 시각화
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i])
+    plt.title("original")
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+    ax = plt.subplot(2, n, i+ 1 + n)
+    plt.imshow(decoded_imgs[i])
+    plt.title("reconstructed")
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
