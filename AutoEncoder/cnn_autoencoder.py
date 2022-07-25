f94427d4c15a3547b9ecda45caaba0b9a85c60d7
kkimlee <40078267+kkimlee@users.noreply.github.com>
2022-07-25 13:21:59 +0900

    Create cnn_autoencoder.py
    CNN 기반 autoencoder

---
+++ AutoEncoder/cnn_autoencoder.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
+
+#%% 데이터 불러오기
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+print(x_train.shape)
+
+#%% 이미지에 노이즈 추가
+noise_factor = 0.2
+x_train_noisy = x_train + noise_factor * tf.random.normal(shape=x_train.shape)
+x_test_noisy = x_test + noise_factor * tf.random.normal(shape=x_test.shape)
+
+x_train_noisy = tf.clip_by_value(x_train_noisy, clip_value_min=0., clip_value_max=1.)
+x_test_noisy = tf.clip_by_value(x_test_noisy, clip_value_min=0., clip_value_max=1.)
+
+#%% 노이즈 이미지 시각화
+n = 10
+plt.figure(figsize=(20, 2))
+for i in range(n):
+    ax = plt.subplot(1, n, i + n)
+    plt.title("original + noise")
+    plt.imshow(tf.squeeze(x_test_noisy[i]))
+    plt.gry()
+plt.show()
+
+#%% 모델 정의
+class Denoise(Model):
+    def __init__(self):
+        super(Denoise, self).__init__()
+        self.encoder = tf.keras.Sequential([
+            layers.Input(shape=(28, 28, 1)),
+            layers.Conv2D(16, (3, 3), activation='relu', padding='same', strides=2),
+            layers.Conv2D(8, (3, 3), activation='relu', padding='same', strides=2)
+        ])
+        
+        self.decoder = tf.keras.Sequential([
+            layers.Conv2DTranspose(8, kernel_size=3, strides=2, activation='relu', padding='same'),
+            layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same'),
+            layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same')
+        ])
+        
+    def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+autoencoder = Denoise()
+
+#%% 모델 초기화
+autoencoder.compile(optimizer='adam', loss=losses.MeanSquredError())
+
+#%% 모델 학습
+autoencoder.fit(x_train_noisy, x_train,
+                epochs=10,
+                shuffle=True,
+                validation_data=(x_test_noisy, x_test))
+
+#%% encoder를 통한 다운샘플링
+autoencoder.encoder.summary()
+
+#%% decoder를 통한 업샘플링
+autoencoder.decoder.summary()
+
+#%% 테스트
+encoded_imgs = autoencder.encoder(x_test).numpy()
