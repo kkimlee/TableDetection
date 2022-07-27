508fe7379564586efa0446860598da1615378ab8
kkimlee <40078267+kkimlee@users.noreply.github.com>
2022-07-27 12:54:12 +0900

    Update table_detection.py

--- GAN/table_detection.py
+++ GAN/table_detection.py
@@ -9,6 +9,7 @@ import PIL
 from tensorflow.keras import layers
 import time
 
+from IPython import display
 #%% 데이터셋 불러오기
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 
@@ -18,7 +19,7 @@ train_images = (train_images - 127.5) / 127.5
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
 
-train_dataset = tf.data.Datset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
 
 #%% 생성자 모델 정의
@@ -75,3 +76,81 @@ def make_discriminator_model():
 discriminator = make_discriminator_model()
 decision = discriminator(generated_image)
 print(decision)
+
+#%% 손실함수
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+#%% 생성자 손실함수 계산
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+#%% 감별자 손실함수 계산
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+#%% 옵티마이저 정의
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+#%% 체크포인트
+checkpoint_dir = './'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+#%% 훈련 루프 정의
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
+
+seed = tf.random.nermal([num_examples_to_generate, noise_dim])
+
+#%% 학습을 위한 그래프 생성
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+    
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+        
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+        
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+        
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+#%% 학습
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+        
+        for image_batch in dataset:
+            train_step(image_batch)
+            
+        display.clear_output(wait=True)
+        generate_and_save_images(generator,
+                                 epoch + 1,
+                                 seed)
+        
+        if (epoch + 1) % 15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+            
+        print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+    
+    display.clear_output(wait=True)
+    generate_and_save_image(generator,
+                            epochs,
+                            seed)
+
+#%% 이미지 생성 및 저
+        
